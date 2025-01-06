1608cc42a4036e8b5a661e0768a9b2a8ed246d51
Anastasia Botsula <nactya.botsula@gmail.com>
2025-01-07 02:47:22 +0300

    feat: add chainMaker solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+  finalChain: '',
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (typeof(value) === 'undefined') {
+      this.chain.push('');
+    } 
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof(position) === 'number' &&
+    position > 0 && 
+    position < this.chain.length) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    } else {
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.finalChain = this.chain.join('~~');
+    this.chain = [];
+    return this.finalChain;
   }
 };
 
